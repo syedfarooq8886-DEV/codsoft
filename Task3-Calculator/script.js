19e445bcc13ba415cc488b99f3637925b3c16b1f
SYED MOHAMMAD FAROOQ <syedfarooq8886@gmail.com>
2026-06-15 22:53:06 +0530

    Create script.js

---
+++ Task3-Calculator/script.js
@@ -0,0 +1,20 @@
+```javascript
+const display = document.getElementById("display");
+
+function appendValue(value){
+    display.value += value;
+}
+
+function clearDisplay(){
+    display.value = "";
+}
+
+function calculate(){
+    try{
+        display.value = eval(display.value);
+    }
+    catch{
+        display.value = "Error";
+    }
+}
+```
